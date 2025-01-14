92c11c384dc5d539a45aeea53997f95fe1c984a1
Amiri12 <amiriphorn@gmail.com>
2025-01-14 11:15:44 -0500

    Update BAJA_RPM.ino

--- BAJA_RPM/BAJA_RPM.ino
+++ BAJA_RPM/BAJA_RPM.ino
@@ -12,11 +12,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   static bool lastState = false;
-  bool currentState = analogRead(A0) < 790;
-
+  bool currentState = analogRead(A6) < 790;
+  int X_Accel = map(anologRead(A2), 0, 1024, 0, 255);
+  int Y_Accel = map(analogRead(A3), 0, 1024, 0, 255);
+  int Z_Accel = map(anaolgRead(A4), 0, 1024, 0, 255);
   if (currentState && !lastState) {
-    if (analogRead(A0)<790){
-      Serial.println(analogRead(A0));
+    if (analogRead(A6)<790){
+      Serial.println(analogRead(A6));
       unsigned long currentTime = micros();
       if (currentTime > timeold) { // Ensure time difference is valid
         rpm = min(3800,(60000000.0 / (currentTime - timeold)));
@@ -32,5 +34,7 @@ void loop() {
   
   out = map(rpm, 0, 3800, 0, 255);
   analogWrite(3, out);
+  analogWrite(5, );
+  
   lastState = currentState;
 }
